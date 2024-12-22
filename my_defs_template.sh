33f43d74617c651a6ddd6d4be0fb48a59aa89092
dodlido <etaysela@gmail.com>
2024-12-22 21:15:34 +0200

    updated my_defs_template.sh to the new format of environment veriables

--- my_defs_template.sh
+++ my_defs_template.sh
@@ -1,18 +1,30 @@
+
+################################################
+### Manual edit of active script starts here ###
+################################################
 # Your github info #
-export git_username="username"
-export git_main_path="git@github.com:username/"
-export git_key_path="wherever you keep your api private key"
+export git_username="git_username"
+export git_main_path="git@github.com:git_username/"
+export git_key_path="path_to_api_key"
+export git_ssh_key_path="path_to_private_ssh_key" # Path to your own private ssh key
+export git_ssh_key_passphrase="pass_phrase" # Passphrase to your own private ssh key
 # Your name #
-export real_username="name"
+export real_username="some_name_to_autograph_releases_with"
 # Paths to your personal storage #
-export rls_dir="some arbitrary path"   # Directory to store releases 
-export home_dir="some arbitrary path"  # Directory to develop 
-export work_dir="some arbitrary path"  # Directory to see simulation results
-export utils_dir="some arbitrary path" # Directory to my_defs.sh, setup.sh and pyvenv
+#export defs_path="/home/etay-sela/design/veri_tools/my_defs.sh"         # Path to this file
+export setup_path="" # Path to this file
+export rls_dir=   "" # Directory to store releases in
+export home_dir=  "" # Directory to develop in 
+export work_dir=  "" # Directory to see simulation results in
+export utils_dir= "" # Directory to my_defs.sh, and tools, py_venv folders
+export venv_dir=  "" # Directory to virtual python environment
 # Tools setup #
-export tools_version="clone to your releases some tag and specify here which tag you used" # Specify the tools version you would like to use
-export tools_dir="${rls_dir}/veri_env/${tools_version}/"
+export tools_dir="${utils_dir}/veri_env/"
+alias setup='source ${setup_path}'
 alias sim='python3 ${tools_dir}sim.py'
 alias get='python3 ${tools_dir}get.py'
 alias add='python3 ${tools_dir}add.py'
 alias rls='python3 ${tools_dir}release.py'
+################################################
+### Manual edit of active script ends here   ###
+################################################
